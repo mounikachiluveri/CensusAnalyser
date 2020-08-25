8a9beb6ac9627b0567ce39467ce63d761930dfd2
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-08-25 14:11:51 +0530

    Mounika Added:Second Test case in india state code when wrong filepath provided it throws exception.

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -11,6 +11,8 @@ public class CensusAnalyserTest {
     private static final String WRONG_FILE_TYPE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String INCORRECT_DELIMITER_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataIncorrectDelimiter.csv";
     private static final String INCORRECT_DATA_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataIncorrectheader.csv";
+    private static final String INDIA_STATE_CODE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
+    private static final String WRONG_STATE_CODE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -70,4 +72,29 @@ public class CensusAnalyserTest {
         }
 
     }
+    @Test
+    public void givenIndiaStateCodeCSVFile_whenProper_shouldReturnsCorrectRecordCount() {
+        ExpectedException expectedException = ExpectedException.none();
+        expectedException.expect(CensusAnalyserException.class);
+
+        try {
+            int numberOfRecords = CensusAnalyser.loadIndiaStateCodeData(INDIA_STATE_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37, numberOfRecords);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_DATA_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaStateCodeCSVFile_whenWrongPath_shouldThrowCustomException() {
+        ExpectedException expectedException = ExpectedException.none();
+        expectedException.expect(CensusAnalyserException.class);
+
+        try {
+            CensusAnalyser.loadIndiaCensusData(WRONG_STATE_CODE_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
 }
+
